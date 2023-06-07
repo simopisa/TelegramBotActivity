b5ff052966ce950e08d2a4da9e9f0f7c1459b886
simopisa <48206660+simopisa@users.noreply.github.com>
2023-06-07 15:32:36 +0200

    Add files via upload

---
+++ Function1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Unidea_Activity_Telegram_bot
+{
+    public class Function1
+    {
+
+        private ProductInfoHeaderValue userAgent;
+        private HttpClient httpClient;
+        private const string IG_BASE_URL = "https://www.instagram.com/api/v1/";
+        private const string CHAT_ID = "-1001714171921";
+
+
+        [FunctionName("Function1")]
+        public async Task Run([TimerTrigger("0 0 10 * * 3")] TimerInfo myTimer, ILogger log)
+        {
+            var _botClient = GetTelegramBotClient();
+
+            var msg = await this.ComposeMessage();
+
+            await _botClient.SendPhotoAsync(CHAT_ID, InputFile.FromUri(msg.displayImage), caption: msg.Caption);
+
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+
+
+
+        //[FunctionName("FunctionProva")]
+        //public async Task RunProva([TimerTrigger("1 * * * * *")] TimerInfo myTimer, ILogger log)
+        //{
+        //    var _botClient = GetTelegramBotClient();
+
+
+        //    var msg = await this.ComposeMessage();
+
+        //    await _botClient.SendPhotoAsync(CHAT_ID, InputFile.FromUri(msg.displayImage), caption: msg.Caption);
+
+        //    log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        //}
+
+
+        public async Task<MessageContent> ComposeMessage()
+        {
+            var userinfo = await this.GetProfileAsync("unidea_tn");
+
+            List<Edge> feed = userinfo.data.user.edge_owner_to_timeline_media.edges;
+
+            MessageContent theOne = feed.Where(x => x.node.edge_media_to_caption.edges[0].node.text.Contains("mercoledì")).Select(x => x.node).Select(x => new MessageContent
+            {
+                Caption = $"📣i nostri amici di unidea_tn vi propongono diverse 💡IDEE💡per questo mercoledì 💙🧡 \n https://instagram.com/p/{x.shortcode}",
+                displayImage = x.thumbnail_src,
+                PostLink = $"https://instagram.com/p/{x.shortcode}",
+            }).FirstOrDefault();
+
+            return theOne;
+        }
+
+
+        #region Instagram
+        public async Task<WebProfileInfoViewModel?> GetProfileAsync(string username)
+        {
+            userAgent = new ProductInfoHeaderValue("(Instagram 219.0.0.12.117 Android)");
+            httpClient = new HttpClient();
+            var url = BuildUrl($"users/web_profile_info/?username={username}");
+            return await this.GetResponseAsync<WebProfileInfoViewModel>(url);
+        }
+        private async Task<T?> GetResponseAsync<T>(Uri requestUri) where T : class
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            request.Headers.UserAgent.Add(userAgent);
+
+
+            HttpResponseMessage? response = await httpClient.SendAsync(request);
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                string? json = response.Content.ReadAsStringAsync().Result;
+                return JsonSerializer.Deserialize<T>(json);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+        private Uri BuildUrl(string relativeurl)
+        {
+            return new Uri($"{IG_BASE_URL}{relativeurl}");
+        }
+        #endregion
+
+
+        private TelegramBotClient GetTelegramBotClient()
+        {
+            var token = Environment.GetEnvironmentVariable("telegramBotApiKey");
+
+            return new TelegramBotClient(token);
+        }
+    }
+}
---
+++ WebProfileInfoViewModel.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidea_Activity_Telegram_bot
+{
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class BiographyWithEntities
+    {
+        public string raw_text { get; set; }
+        public List<object> entities { get; set; }
+    }
+
+    public class BioLink
+    {
+        public string title { get; set; }
+        public string lynx_url { get; set; }
+        public string url { get; set; }
+        public string link_type { get; set; }
+    }
+
+    public class Data
+    {
+        public User user { get; set; }
+    }
+
+    public class Dimensions
+    {
+        public int height { get; set; }
+        public int width { get; set; }
+    }
+
+    public class Edge
+    {
+        public Node node { get; set; }
+    }
+
+    public class EdgeFelixVideoTimeline
+    {
+        public int count { get; set; }
+        public PageInfo page_info { get; set; }
+        public List<object> edges { get; set; }
+    }
+
+    public class EdgeFollow
+    {
+        public int count { get; set; }
+    }
+
+    public class EdgeFollowedBy
+    {
+        public int count { get; set; }
+    }
+
+    public class EdgeLikedBy
+    {
+        public int count { get; set; }
+    }
+
+    public class EdgeMediaCollections
+    {
+        public int count { get; set; }
+        public PageInfo page_info { get; set; }
+        public List<object> edges { get; set; }
+    }
+
+    public class EdgeMediaPreviewLike
+    {
+        public int count { get; set; }
+    }
+
+    public class EdgeMediaToCaption
+    {
+        public List<Edge> edges { get; set; }
+    }
+
+    public class EdgeMediaToComment
+    {
+        public int count { get; set; }
+    }
+
+    public class EdgeMediaToTaggedUser
+    {
+        public List<object> edges { get; set; }
+    }
+
+    public class EdgeMutualFollowedBy
+    {
+        public int count { get; set; }
+        public List<object> edges { get; set; }
+    }
+
+    public class EdgeOwnerToTimelineMedia
+    {
+        public int count { get; set; }
+        public PageInfo page_info { get; set; }
+        public List<Edge> edges { get; set; }
+    }
+
+    public class EdgeRelatedProfiles
+    {
+        public List<Edge> edges { get; set; }
+    }
+
+    public class EdgeSavedMedia
+    {
+        public int count { get; set; }
+        public PageInfo page_info { get; set; }
+        public List<object> edges { get; set; }
+    }
+
+    public class EdgeSidecarToChildren
+    {
+        public List<Edge> edges { get; set; }
+    }
+
+    public class Node
+    {
+        public string __typename { get; set; }
+        public string id { get; set; }
+        public string shortcode { get; set; }
+        public Dimensions dimensions { get; set; }
+        public string display_url { get; set; }
+        public EdgeMediaToTaggedUser edge_media_to_tagged_user { get; set; }
+        public object fact_check_overall_rating { get; set; }
+        public object fact_check_information { get; set; }
+        public object gating_info { get; set; }
+        public SharingFrictionInfo sharing_friction_info { get; set; }
+        public object media_overlay_info { get; set; }
+        public object media_preview { get; set; }
+        public Owner owner { get; set; }
+        public bool is_video { get; set; }
+        public bool has_upcoming_event { get; set; }
+        public object accessibility_caption { get; set; }
+        public EdgeMediaToCaption edge_media_to_caption { get; set; }
+        public EdgeMediaToComment edge_media_to_comment { get; set; }
+        public bool comments_disabled { get; set; }
+        public int taken_at_timestamp { get; set; }
+        public EdgeLikedBy edge_liked_by { get; set; }
+        public EdgeMediaPreviewLike edge_media_preview_like { get; set; }
+        public object location { get; set; }
+        public object nft_asset_info { get; set; }
+        public string thumbnail_src { get; set; }
+        public List<ThumbnailResource> thumbnail_resources { get; set; }
+        public List<object> coauthor_producers { get; set; }
+        public List<object> pinned_for_users { get; set; }
+        public bool viewer_can_reshare { get; set; }
+        public EdgeSidecarToChildren edge_sidecar_to_children { get; set; }
+        public string text { get; set; }
+        public string full_name { get; set; }
+        public bool is_private { get; set; }
+        public bool is_verified { get; set; }
+        public string profile_pic_url { get; set; }
+        public string username { get; set; }
+    }
+
+    public class Owner
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+    }
+
+    public class PageInfo
+    {
+        public bool has_next_page { get; set; }
+        public object end_cursor { get; set; }
+    }
+
+    public class WebProfileInfoViewModel
+    {
+        public Data data { get; set; }
+        public string status { get; set; }
+    }
+
+    public class SharingFrictionInfo
+    {
+        public bool should_have_sharing_friction { get; set; }
+        public object bloks_app_url { get; set; }
+    }
+
+    public class ThumbnailResource
+    {
+        public string src { get; set; }
+        public int config_width { get; set; }
+        public int config_height { get; set; }
+    }
+
+    public class User
+    {
+        public string biography { get; set; }
+        public List<BioLink> bio_links { get; set; }
+        public BiographyWithEntities biography_with_entities { get; set; }
+        public bool blocked_by_viewer { get; set; }
+        public object restricted_by_viewer { get; set; }
+        public bool country_block { get; set; }
+        public string eimu_id { get; set; }
+        public string external_url { get; set; }
+        public string external_url_linkshimmed { get; set; }
+        public EdgeFollowedBy edge_followed_by { get; set; }
+        public string fbid { get; set; }
+        public bool followed_by_viewer { get; set; }
+        public EdgeFollow edge_follow { get; set; }
+        public bool follows_viewer { get; set; }
+        public string full_name { get; set; }
+        public object group_metadata { get; set; }
+        public bool has_ar_effects { get; set; }
+        public bool has_clips { get; set; }
+        public bool has_guides { get; set; }
+        public bool has_channel { get; set; }
+        public bool has_blocked_viewer { get; set; }
+        public int highlight_reel_count { get; set; }
+        public bool has_requested_viewer { get; set; }
+        public bool hide_like_and_view_counts { get; set; }
+        public string id { get; set; }
+        public bool is_business_account { get; set; }
+        public bool is_professional_account { get; set; }
+        public bool is_supervision_enabled { get; set; }
+        public bool is_guardian_of_viewer { get; set; }
+        public bool is_supervised_by_viewer { get; set; }
+        public bool is_supervised_user { get; set; }
+        public bool is_embeds_disabled { get; set; }
+        public bool is_joined_recently { get; set; }
+        public object guardian_id { get; set; }
+        public object business_address_json { get; set; }
+        public string business_contact_method { get; set; }
+        public object business_email { get; set; }
+        public object business_phone_number { get; set; }
+        public object business_category_name { get; set; }
+        public object overall_category_name { get; set; }
+        public object category_enum { get; set; }
+        public string category_name { get; set; }
+        public bool is_private { get; set; }
+        public bool is_verified { get; set; }
+        public bool is_verified_by_mv4b { get; set; }
+        public EdgeMutualFollowedBy edge_mutual_followed_by { get; set; }
+        public string profile_pic_url { get; set; }
+        public string profile_pic_url_hd { get; set; }
+        public bool requested_by_viewer { get; set; }
+        public bool should_show_category { get; set; }
+        public bool should_show_public_contacts { get; set; }
+        public bool show_account_transparency_details { get; set; }
+        public object transparency_label { get; set; }
+        public string transparency_product { get; set; }
+        public string username { get; set; }
+        public object connected_fb_page { get; set; }
+        public List<object> pronouns { get; set; }
+        public EdgeFelixVideoTimeline edge_felix_video_timeline { get; set; }
+        public EdgeOwnerToTimelineMedia edge_owner_to_timeline_media { get; set; }
+        public EdgeSavedMedia edge_saved_media { get; set; }
+        public EdgeMediaCollections edge_media_collections { get; set; }
+        public EdgeRelatedProfiles edge_related_profiles { get; set; }
+    }
+
+
+}
